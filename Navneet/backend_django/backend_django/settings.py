786d02bbc5da7ef3ee28aff8ab747ac5f40aa1d8
Navneet Singh <snavneet20@iitk.ac.in>
2021-07-25 00:26:20 +0530

    created CRUD API

--- Navneet/backend_django/backend_django/settings.py
+++ Navneet/backend_django/backend_django/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notesAPI.apps.NotesapiConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
