3c4f1192823214f79b942b21ec6f87254f644552
subodhkum20 <subodhkum20@iitk.ac.in>
2021-07-26 01:07:24 +0530

    uses mongo cluster

--- note_server/app.js
+++ note_server/app.js
@@ -4,17 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var config = require('./config')
-// var http = require('http');
 var mongoose = require('mongoose');
-// const { MongoClient } = require('mongodb');
 const uri = `mongodb+srv://subodhkumar:${config.password}@cluster0.gm0lg.mongodb.net/test?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   console.log("atlas connected")
-//   // perform actions on the collection object
-//   client.close();
-// });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,8 +14,6 @@ var saveRouter = require('./routes/saveNote');
 var app = express();
 
 
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -32,7 +21,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
