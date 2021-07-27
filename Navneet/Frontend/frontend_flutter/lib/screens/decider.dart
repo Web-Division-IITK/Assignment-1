9d080ea1e657c82c021e94da35c5e5967337e56c
Navneet Singh <snavneet20@iitk.ac.in>
2021-07-28 00:30:54 +0530

    api endpoint

---
+++ Navneet/Frontend/frontend_flutter/lib/screens/decider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'authentication/authenticate.dart';
+
+class Decider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LoginPage();
+  }
+}
