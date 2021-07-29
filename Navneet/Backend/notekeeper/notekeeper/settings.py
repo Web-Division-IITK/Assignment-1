84486ef3fae21c997737ee5555aafa3a38d794d3
Navneet Singh <snavneet20@iitk.ac.in>
2021-07-29 16:06:45 +0530

    CORS error issue resolved

--- Navneet/Backend/notekeeper/notekeeper/settings.py
+++ Navneet/Backend/notekeeper/notekeeper/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notes',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
 ]
@@ -50,8 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
-
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
 ROOT_URLCONF = 'notekeeper.urls'
 
 TEMPLATES = [
