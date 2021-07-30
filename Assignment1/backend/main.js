c50f12df7060bab2aa9fe22b76a66a0c5ce5eb5f
Arpit Kumar Rai <arpitkr20@iitk.ac.in>
2021-07-30 22:16:59 +0530

    Modify request method for /view to POST

--- Assignment1/backend/main.js
+++ Assignment1/backend/main.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const verify = require('./middleware/verify');
 const register = require('./routeHandler/register');
 const view = require('./routeHandler/view')
@@ -7,15 +8,16 @@ const add = require('./routeHandler/addNote')
 const login = require('./routeHandler/login');
 const deleteNote = require('./routeHandler/delete');
 app.use(express.json());
+app.use(cors());
 
 app.post('/login',login)
 app.post("/register",register);
 app.use(verify);
-app.get("/view",view.view);
+app.post("/view",view.view);
 app.post('/addNotes',add);
 app.post('/delete',deleteNote);
 // app.post("/login",auth.login);
 
-app.listen(3000);
+app.listen(5000);
 
 
