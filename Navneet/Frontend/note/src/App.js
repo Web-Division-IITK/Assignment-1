277ea790dde6ad0505722c4970c169506cf320e6
Navneet Singh <snavneet20@iitk.ac.in>
2021-07-31 02:27:48 +0530

    fetching notes from backend and rendering

--- Navneet/Frontend/note/src/App.js
+++ Navneet/Frontend/note/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import axios from "axios";
+import AddNote from "./components/getter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todoList: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://localhost:8000/api/notes/');
+      const todoList = await res.json();
+      this.setState({
+        todoList
+      });
+    } catch (e) {
+      console.log(e);
+  }
+  }
+
+
+  //Responsible for saving the task
+  handleSubmit = item => {
+    if (item.id) {
+      axios
+        .put(`http://localhost:8000/api/todos/${item.id}/`, item)
+      return;  
+    }
+    axios
+      .post("http://localhost:8000/api/todos/", item)
+  };
+
+  createItem = () => {
+    const item = {title: "", description: ""};
+  };
+
+  renderItems = () => {
+    const newItems = this.state.todoList;
+    return newItems.map(item => (
+      <li 
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span 
+          title={item.description}
+          >
+            {item.title}
+          </span>
+      </li>
+    ));
+  };
+
+  render() {
+    return (
+      <main className="content">
+      <h1 className="text-white text-uppercase text-center my-4">Note Keeper</h1>
+      <div className="row">
+        <div className="col-md-6 col-sm-10 mx-auto p-0">
+          <div className="card p-3">
+            <div className="">
+              <button onClick={this.createItem} className="btn btn-success">Add Note</button>
+            </div>
+            <ul className="list-group list-group-flush">
+              {this.renderItems()}
+            </ul>
+          </div>
+        </div>
+      </div>
+      {this.state.modal ? (
+        <AddNote
+          onSave={this.handleSubmit}
+        />
+      ): null}
+    </main>
+    )
+  }
 }
 
 export default App;
---
+++ Navneet/Frontend/note/src/components/getter.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
+
+const base_url = "http://127.0.0.1:8000/";
+
+const AddNote = () => {
+
+  const { title1,setTitle } = useState("");
+  const {description1,setDescription} =useState("");
+
+  const onSave = async (e) =>{
+    await axios
+    .post(base_url+"api/notes/",
+    {
+        title: title1,
+        description: description1,
+    })
+    .then((res) => {
+        alert("note saved suucessfully");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("error while saving");
+      });
+  }
+  return (
+    <Modal >
+      <ModalHeader >Notes</ModalHeader>
+      <ModalBody>
+        <Form>
+          <FormGroup>
+            <Label for="title">Title</Label>
+            <Input
+              required
+              valid
+              type="text"
+              name="title"
+              placeholder="note title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="description">Description</Label>
+            <Input
+              type="text"
+              name="description"
+              placeholder="note description"
+              onChange={(e)=>setDescription(e.target.value)}
+            />
+          </FormGroup>
+        </Form>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="success" 
+        onClick={(e) => onSave(e)}>
+          Save
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+export default AddNote;
