60a0d087caf11bd37964af62bfde1c6e100122ee
Arpit Kumar Rai <arpitkr20@iitk.ac.in>
2021-07-30 22:18:08 +0530

    Modify addNote end point to link with front end

--- Assignment1/backend/routeHandler/addNote.js
+++ Assignment1/backend/routeHandler/addNote.js
@@ -1,5 +1,3 @@
-const checkEmail = require('./view').checkEmail;
-
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 let db = null;
@@ -13,13 +11,18 @@ async function addNotes(req,res){
     let result = await dbo.collection("Customers").find({email : req.body.email}).toArray();
 
     if(result[0].notes){
-        result[0].notes.push(req.body.data);
+        result[0].notes.unshift(req.body.data);
         await dbo.collection("Customers").updateOne({email : req.body.email},{$set : {notes : result[0].notes}});
+        let index = parseInt(req.body.index);
+        if(!(index===-1)){
+            result[0].notes.splice(index+1,1);
+            await dbo.collection("Customers").updateOne({email : req.body.email},{$set : {notes : result[0].notes}});
+        }
     }
     else{
         await dbo.collection("Customers").updateOne({email : req.body.email},{$set : {notes : new Array(req.body.data)}});
     }
-    return res.status(200).json({"msg" : "Notes added successfully"});
+    return res.status(200).json({"msg" : "Notes added successfully","data":result[0].notes});
 }
 
 module.exports = addNotes;
