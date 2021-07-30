08c434cedf9e25267114d2d836573341b7b6b7a6
Arpit Kumar Rai <arpitkr20@iitk.ac.in>
2021-07-30 22:19:29 +0530

    Modify register end point to link with front end

--- Assignment1/backend/routeHandler/register.js
+++ Assignment1/backend/routeHandler/register.js
@@ -15,6 +15,7 @@ async function register(req,res){
     let dbo = db.db("User");
     let query = {email : req.body.email};
     let result = await dbo.collection("Customers").find(query).toArray();
+    res.set('Access-Control-Allow-Origin', '*');
     if(result.length>0){
         return res.status(401).json({"msg": "Email already in use"});
     }
@@ -23,7 +24,7 @@ async function register(req,res){
     let data = {email : req.body.email, password : req.body.password}
     await dbo.collection("Customers").insertOne(data);
     let token = jwt.sign({email: req.body.email},config.secret,{expiresIn : 86400});
-    return res.status(200).json({"auth" :true,"msg":"User added successfully","token":token});
+    return res.status(200).json({"auth" :true,"msg":"User added successfully",token:token});
 }
 
 module.exports = register;
