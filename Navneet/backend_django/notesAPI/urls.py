1f5c459eeae9ceb1b0f73c910617061de9c942b2
Navneet Singh <snavneet20@iitk.ac.in>
2021-08-01 18:19:54 +0530

    deleted flutter app

--- Navneet/backend_django/notesAPI/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .api import NoteViewSets
-
-router = routers.DefaultRouter()
-router.register('api/notesAPI',NoteViewSets,'notesAPI')
-
-urlpatterns = router.urls
