8217d7ad2137eb820b5519decfdb7c511d1f45b0
Sahil Singh <sahilsingh08062001@gmail.com>
2021-08-06 02:47:38 +0530

    ui updates

--- Frontend/noted/lib/Notes.dart
+++ Frontend/noted/lib/Notes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'addNotes.dart';
 import 'note_model.dart';
 
 class NotesScreen extends StatefulWidget {
@@ -20,6 +21,7 @@ class _NotesScreenState extends State<NotesScreen>
     _tabController = TabController(initialIndex: 0, length: 3, vsync: this);
   }
 
+ 
   Widget _buildCategoryCard(int index, String title, int count) {
     return GestureDetector(
       onTap: () {
@@ -84,173 +86,178 @@ class _NotesScreenState extends State<NotesScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          SizedBox(height: 40.0),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  height: 50.0,
-                  width: 50.0,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/user.png'),
+      body: Container(
+        color: Color(0xFFD9F0FC),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(height: 40.0),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/user.png'),
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                ),
-                SizedBox(width: 10,),
-                Text(
-                  'NOTED',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 40.0),
-          Container(
-            height: 200.0,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length + 1,
-              itemBuilder: (BuildContext context, int index) {
-                if (index == 0) {
-                  return SizedBox(width: 20.0);
-                }
-                return _buildCategoryCard(
-                  index - 1,
-                  categories.keys.toList()[index - 1],
-                  categories.values.toList()[index - 1],
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 15.0),
-            child: TabBar(
-              controller: _tabController,
-              labelColor: Colors.black,
-              unselectedLabelColor: Colors.blueGrey,
-              indicatorColor: Color(0xFF417BFB),
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorWeight: 4.0,
-              isScrollable: true,
-              tabs: <Widget>[
-                Tab(
-                  child: Text(
-                    'Notes',
+                  SizedBox(width: 10,),
+                  Text(
+                    'NOTED',
+                    textAlign: TextAlign.center,
                     style: TextStyle(
-                      fontSize: 22.0,
+                      fontSize: 28.0,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                Tab(
-                  child: Text(
-                    'Important',
+                ],
+              ),
+            ),
+            SizedBox(height: 40.0),
+            Container(
+              height: 200.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length + 1,
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0) {
+                    return SizedBox(width: 20.0);
+                  }
+                  return _buildCategoryCard(
+                    index - 1,
+                    categories.keys.toList()[index - 1],
+                    categories.values.toList()[index - 1],
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 15.0),
+              child: TabBar(
+                controller: _tabController,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.blueGrey,
+                indicatorColor: Color(0xFF417BFB),
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorWeight: 4.0,
+                isScrollable: true,
+                tabs: <Widget>[
+                  Tab(
+                    child: Text(
+                      'Notes',
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      'Important',
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      'Performed',
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30.0),
+              padding: EdgeInsets.all(30.0),
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F7FB),
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        notes[0].title,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        _timeFormatter.format(notes[0].date),
+                        style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 15.0),
+                  Text(
+                    notes[0].content,
                     style: TextStyle(
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
-                ),
-                Tab(
-                  child: Text(
-                    'Performed',
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  SizedBox(height: 15.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Text(
+                        _dateFormatter.format(notes[0].date),
+                        style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      // Container(
+                      //   height: 50.0,
+                      //   width: 50.0,
+                      //   decoration: BoxDecoration(
+                      //     color: Color(0xFF417BFB),
+                      //     borderRadius: BorderRadius.circular(15.0),
+                      //   ),
+                      //   child: Icon(
+                      //     Icons.location_on,
+                      //     color: Colors.white,
+                      //   ),
+                      // ),
+                    ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 20.0),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 30.0),
-            padding: EdgeInsets.all(30.0),
-            decoration: BoxDecoration(
-              color: Color(0xFFF5F7FB),
-              borderRadius: BorderRadius.circular(30.0),
-            ),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      notes[0].title,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      _timeFormatter.format(notes[0].date),
-                      style: TextStyle(
-                        color: Colors.blueGrey,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 15.0),
-                Text(
-                  notes[0].content,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(height: 15.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    Text(
-                      _dateFormatter.format(notes[0].date),
-                      style: TextStyle(
-                        color: Colors.blueGrey,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    // Container(
-                    //   height: 50.0,
-                    //   width: 50.0,
-                    //   decoration: BoxDecoration(
-                    //     color: Color(0xFF417BFB),
-                    //     borderRadius: BorderRadius.circular(15.0),
-                    //   ),
-                    //   child: Icon(
-                    //     Icons.location_on,
-                    //     color: Colors.white,
-                    //   ),
-                    // ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 20.0),
-        ],
+            SizedBox(height: 20.0),
+          ],
+        ),
       ),
       floatingActionButton: new FloatingActionButton(
       elevation: 0.0,
       child: new Icon(Icons.add),
       backgroundColor: new Color(0xFF417BFB),
-      onPressed: (){}
+      onPressed: (){
+        Navigator.push(context,MaterialPageRoute(builder: (context) => AddNotes()),);
+      }
     ),
     );
   }
---
+++ Frontend/noted/lib/addNotes.dart
@@ -0,0 +1,154 @@
+import 'package:favorite_button/favorite_button.dart';
+import 'package:flutter/material.dart';
+class AddNotes extends StatefulWidget {
+
+
+   const AddNotes({ Key? key }) : super(key: key);
+
+  @override
+  _AddNotesState createState() => _AddNotesState();
+}
+
+class _AddNotesState extends State<AddNotes> {
+  int _value=0;
+  final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+    onPrimary: Colors.white,
+    primary: Color(0xFF417BFB),
+    minimumSize: Size(88, 36),
+    padding: EdgeInsets.symmetric(horizontal: 16),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(5)),
+    ),
+  );
+
+   Widget _showChoiceChip(String name, int selectNumber){
+     return ChoiceChip(label: Text(name),
+     selected: this._value==selectNumber,
+     onSelected: (bool selected){
+       this.setState((){
+            _value = selected ? selectNumber : 0;
+          });
+     });
+   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Notes',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+        ),
+        backgroundColor: Color(0xFF417BFB),
+      ), 
+
+      body: SingleChildScrollView(
+        child: Container(
+          color: Color(0xFFD9F0FC),
+          child: Column(
+            children: <Widget>[
+              Container(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter heading'
+                  ),
+                ),
+              ),
+            ),
+              
+            Container(
+              child: Padding(
+                padding: EdgeInsets.all(32.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black)
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Your note goes here'
+                    ),
+                   ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+              
+            Center(
+              child: Wrap(
+                children: <Widget>[
+                  
+                 SizedBox(width: 20,),
+                 _showChoiceChip('Work',1),
+                 SizedBox(
+                        width: 20,
+                      ),
+                 _showChoiceChip('Home',2),
+                 SizedBox(
+                        width: 20,
+                      ),
+                 _showChoiceChip('Others',3),
+                 SizedBox(
+                        width: 20,
+                      ),
+               
+                FavoriteButton(
+                  isFavorite: true,
+                        valueChanged: (_isFavourite) {
+                          print('Is Favourite $_isFavourite)');
+                          if(_isFavourite==true)
+                          {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text("Marked as Favorite"),
+                              backgroundColor: Color(0xFF417BFB),
+                              duration: Duration(milliseconds: 500),
+                            ));
+                          }
+                          else
+                          {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text("Unmarked from Favorite"),
+                              backgroundColor: Color(0xFF417BFB),
+                              duration: Duration(milliseconds: 500),
+                            ));
+                          }
+                        },
+                ),
+                ],
+              ),
+            ),
+            SizedBox(height: 50,),
+            SizedBox(
+              height: 50,
+              width: 100,
+              child: ElevatedButton(
+                onPressed: (){}, 
+                child: Text('SAVE',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+                style: raisedButtonStyle,),
+            ),
+            // SizedBox(height: 20,),
+            Image.asset('assets/images/NOTED.png')
+          ],
+          ),
+        ),
+      )    
+    );
+  }
+}
+
+void setState(Null Function() param0) {
+}
+
+
+
